51067bbc458107384b62b433449d2d6bc57b47a8
Geoff Hing <geoffhing@gmail.com>
2017-01-19 18:59:46 -0600

    Add formatted versions of vote percentages
    Carto infowindows don't provide any support to reformat values. So, we
    have to add columns with the formatted versions.  Here's the SQL to
    create those columns.

--- add_calculated_columns.sql
+++ add_calculated_columns.sql
@@ -8,6 +8,15 @@ UPDATE pres_election_data_2016n_appalachia
 SET clinton_pct = clinton / total_vote;
 
 ALTER TABLE pres_election_data_2016n_appalachia
+DROP COLUMN IF EXISTS clinton_pct_fmt;
+
+ALTER TABLE pres_election_data_2016n_appalachia
+ADD COLUMN clinton_pct_fmt text;
+
+UPDATE pres_election_data_2016n_appalachia
+SET clinton_pct_fmt = to_char(clinton_pct * 100, '99.9') 
+
+ALTER TABLE pres_election_data_2016n_appalachia
 DROP COLUMN IF EXISTS trump_pct;
 
 ALTER TABLE pres_election_data_2016n_appalachia
@@ -15,6 +24,15 @@ ADD COLUMN trump_pct numeric;
 
 UPDATE pres_election_data_2016n_appalachia
 SET trump_pct = trump / total_vote;
+
+ALTER TABLE pres_election_data_2016n_appalachia
+DROP COLUMN IF EXISTS trump_pct_fmt;
+
+ALTER TABLE pres_election_data_2016n_appalachia
+ADD COLUMN trump_pct_fmt text;
+
+UPDATE pres_election_data_2016n_appalachia
+SET trump_pct_fmt = to_char(trump_pct * 100, '99.9') 
 
 ALTER TABLE pres_election_data_2016n_appalachia
 DROP COLUMN IF EXISTS trump_margin;
